6d76a417741f389b123dce4ff82ff4c2718a9d7c
Denotess <tian.istenic34@gmail.com>
2025-11-13 22:54:12 +0100

    added favicon and image

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,13 @@ const themeStore = useThemeStore()
   <div class="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
     <header class="bg-white dark:bg-gray-800 shadow-sm">
       <div class="container mx-auto px-3 sm:px-4 py-4 sm:py-6 flex items-center justify-between gap-3">
-        <h1 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-gray-900 dark:text-white">UM Calendar</h1>
+        <div class="flex items-center gap-3">
+          <img src="../src/assets/UM-calendar-logo.png" class="h-[4xem] object-contain">
+
+          <h1 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-gray-900 dark:text-white">
+            UM Calendar
+          </h1>
+        </div>
 
         <button @click="themeStore.toggleTheme"
           class="p-2 rounded-lg bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors flex-shrink-0"
